49e911e437d528265d3278bc2e92621b48385872
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2021-08-11 20:27:08 -0500

    delete todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 
 
 function App() {
 
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    let savedTodos = localStorage.getItem("todos")
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
+
   const [todo, setTodo] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
 
   const handleInputChange = (e) => {
     setTodo(e.target.value);
@@ -14,9 +26,14 @@ function App() {
   const handleFormSubmit = (e) => {
     e.preventDefault();
     if(todo !== ""){
-      setTodos([ ...todos, { id: todos.length + 1, text: todo.trim() }])
+      setTodos([ ...todos, { id: todos.length + 1, text: todo.trim() }]);
     }
     setTodo("");
+  }
+
+  const handleDeleteClick = (id) => {
+    const newTodosList = todos.filter(todo => todo.id !== id);
+    setTodos(newTodosList)
   }
 
   return (
@@ -34,7 +51,10 @@ function App() {
 
       <ul className="todo-list">
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => handleDeleteClick(todo.id)}>Delete</button>
+          </li>
         ))}
       </ul>
 
