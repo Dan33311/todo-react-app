6a148c5fe28a7d30aba8a6496522a04c92814fe5
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2021-12-15 22:14:40 -0500

    add one more input in add form

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -4,8 +4,9 @@ const AddTodoForm = ({ todo, onAddFormSubmit, onAddInputChange }) => {
   return (  
     <form className="input-form" onSubmit={onAddFormSubmit}>
       {/* <h2>Add ToDo</h2> */}
-      <label htmlFor="todo">Add Todo : </label>
+      <label className="label-1" htmlFor="todo">Add Todo : </label>
       <input 
+        className="input-1"
         name="todo"
         type="text"
         placeholder="Create a new todo"
@@ -13,7 +14,21 @@ const AddTodoForm = ({ todo, onAddFormSubmit, onAddInputChange }) => {
         value={todo}
         onChange={onAddInputChange}
       />   
-      <button type="submit" className="add-button"><img src={addImage} alt="add"/></button>
+      <button type="submit" className="add-button"><img className="form-img-1" src={addImage} alt="add"/></button>
+      <div className="decoration">|</div>
+      
+
+      <label className="label-2" htmlFor="todo">Number : </label>
+      <input 
+        className="input-2"
+        name="todo"
+        type="number"
+        placeholder="phone number"
+        maxLength="24"
+        value={todo}
+        onChange={onAddInputChange}
+      />   
+      <button type="submit" className="add-button-2"><img className="form-img-2" src={addImage} alt="add"/></button>
       <div className="decoration">|</div>
     </form>
   );
